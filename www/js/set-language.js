240d56bf643189417a065ce8600abc0ceb48b611
robinasplund <r.asplund@mail.com>
2018-10-18 15:05:06 +0200

    some changes , set language function etc. etc.

--- www/js/set-language.js
+++ www/js/set-language.js
@@ -1,2 +1,12 @@
 
 
+let language="swedish";
+
+$('.svflag').click(function(){
+   language="swedish";
+   $.getJSON('/json/startpage.json', write);
+});
+$('.ukflag').click(function(){
+    language="english";
+    $.getJSON('/json/startpage.json', write);
+});
--- www/js/write-startpage-json.js
+++ www/js/write-startpage-json.js
@@ -1,14 +1,4 @@
 
-let language="swedish";
-
-$('.svflag').click(function(){
-   language="swedish";
-   $.getJSON('/json/startpage.json', write);
-});
-$('.ukflag').click(function(){
-    language="english";
-    $.getJSON('/json/startpage.json', write);
-});
 
 function write(data){
 
