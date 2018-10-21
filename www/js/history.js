b4bfe7f4b7b6668ee22e39ec255b9f2e20959448
sunsneezer <xil_wong@hotmail.com>
2018-10-21 17:42:08 +0200

    made changes in history.js

--- www/js/history.js
+++ www/js/history.js
@@ -3,28 +3,24 @@ $.getJSON('/json/history.json', languageToggle);
 
 function languageToggle(translate){
 
-	let wholeTextUL = $('<ul/>');
 	for( let language in translate){
 
 		let lang = translate[language];
 
-		let textUL = $('<ul class="' + language + '"/>');
+		let textUL1 = $('<ul class="' + language + '"/>');
+		let textUL2 = $('<ul class="' + language + '"/>');
 
-		for ( text of lang){
 
-			textUL.append('<p>' + text + '</p>');
+		textUL1.append('<p>' + lang[0] + '</p>');
+		textUL2.append('<p>' + lang[1] + '</p>');
 
-			wholeTextUL.append(textUL);
-
-		}
+		
+		$('.text1').append(textUL1);
+		$('.text2').append(textUL2);
 
 	}
 
-
-	$('.text1').append(wholeTextUL);
-
 	$('.en').hide();
-
 
 	$('.svflag').click(function(){
 		$('.sv').show();
