822f924c85abb03bb6249ffee8101d7b90b87fbd
robinasplund <r.asplund@mail.com>
2018-10-22 14:16:24 +0200

    changed text

--- www/js/change-header-lang.js
+++ www/js/change-header-lang.js
@@ -1,9 +1,9 @@
 
 $('.svflag').click(function(){
     $('h1 span:first-child').text('Detta är');
-    rotatingText.toRotate = ['breakout', 'BREAKOUT'];
+    rotatingText.toRotate = ['breakout online', 'BREAKOUT ONLINE'];
 });
 $('.ukflag').click(function(){
      $('h1 span:first-child').text('This is');
-     rotatingText.toRotate = ['breakout', 'BREAKOUT'];
+     rotatingText.toRotate = ['breakout breakout', 'BREAKOUT ONLINE'];
 });
