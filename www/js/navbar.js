ba0d3c3e4f4d56594585f4c71fde4f904d08e59c
SimonPalmgren <simon.palmgren91@gmail.com>
2018-10-24 09:12:35 +0200

    resized logo and lined it with nav-links

--- www/js/navbar.js
+++ www/js/navbar.js
@@ -12,35 +12,41 @@ function navbarfunction(language_data) {
     let ul_en = $('<ul/>').attr('class','navbar-nav');
 
     /*Create four empty sorted lists (will add the idividual items (higscor, historia etc)into these lists. */
-    let navitem1_sv = $('<li/>').attr('class','nav-item');
-    let navitem2_sv = $('<li/>').attr('class', 'nav-item');
-    let navitem3_sv = $('<li/>').attr('class', 'nav-item');
-    let navitem4_sv = $('<li/>').attr('class', 'nav-item');
+    let navitem1_sv = $('<li/>').attr('class','nav-item nav-margin');
+    let navitem2_sv = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem3_sv = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem4_sv = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem5_sv = $('<li/>').attr('class', 'nav-item icon-resize')
 
     /*Create four empty sorted lists (will add the idividual items (higscor, history etc)into these lists. */
-    let navitem1_en = $('<li/>').attr('class', 'nav-item');
-    let navitem2_en = $('<li/>').attr('class', 'nav-item');
-    let navitem3_en = $('<li/>').attr('class', 'nav-item');
-    let navitem4_en = $('<li/>').attr('class', 'nav-item');
+    let navitem1_en = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem2_en = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem3_en = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem4_en = $('<li/>').attr('class', 'nav-item nav-margin');
+    let navitem5_en = $('<li/>').attr('class', 'nav-item icon-resize')
 
     /* Append adds the text stored in language-date (taken from JSON file)and html-code to the navbar item (empty list)*/
-    navitem1_sv.append('<a class="nav-link" href="' + language_data[0].link  + '"><i class="' + language_data[0].icon + '"></i>' + language_data[0].title.sv + '</a>');
-    navitem2_sv.append('<a class="nav-link" href="' + language_data[1].link  + '"><i class="' + language_data[1].icon + '"></i>' + language_data[1].title.sv + '</a>');
-    navitem3_sv.append('<a class="nav-link" href="' + language_data[2].link  + '"><i class="' + language_data[2].icon + '"></i>' + language_data[2].title.sv + '</a>');
-    navitem4_sv.append('<a class="nav-link" href="' + language_data[3].link  + '"><i class="' + language_data[3].icon + '"></i>' + language_data[3].title.sv + '</a>');
+    navitem5_sv.append('<a class="nav-link" href="' + language_data[0].link  + '"><i class="' + language_data[0].icon + '"></i>' + language_data[0].title.sv + '</a>');
+    navitem1_sv.append('<a class="nav-link" href="' + language_data[1].link  + '"><i class="' + language_data[1].icon + '"></i>' + language_data[1].title.sv + '</a>');
+    navitem2_sv.append('<a class="nav-link" href="' + language_data[2].link  + '"><i class="' + language_data[2].icon + '"></i>' + language_data[2].title.sv + '</a>');
+    navitem3_sv.append('<a class="nav-link" href="' + language_data[3].link  + '"><i class="' + language_data[3].icon + '"></i>' + language_data[3].title.sv + '</a>');
+    navitem4_sv.append('<a class="nav-link" href="' + language_data[4].link  + '"><i class="' + language_data[4].icon + '"></i>' + language_data[4].title.sv + '</a>');
 
     /* Append adds the text stored in language-date (taken from JSON file)and html-code to the navbar item (empty list)*/
-    navitem1_en.append('<a class="nav-link" href="' + language_data[0].link  + '"><i class="' + language_data[0].icon + '"></i>' + language_data[0].title.en + '</a>');
-    navitem2_en.append('<a class="nav-link" href="' + language_data[1].link  + '"><i class="' + language_data[1].icon + '"></i>' + language_data[1].title.en + '</a>');
-    navitem3_en.append('<a class="nav-link" href="' + language_data[2].link  + '"><i class="' + language_data[2].icon + '"></i>' + language_data[2].title.en + '</a>');
-    navitem4_en.append('<a class="nav-link" href="' + language_data[3].link  + '"><i class="' + language_data[3].icon + '"></i>' + language_data[3].title.en + '</a>');
+    navitem5_en.append('<a class="nav-link" href="' + language_data[0].link  + '"><i class="' + language_data[0].icon + '"></i>' + language_data[0].title.en + '</a>');
+    navitem1_en.append('<a class="nav-link" href="' + language_data[1].link  + '"><i class="' + language_data[1].icon + '"></i>' + language_data[1].title.en + '</a>');
+    navitem2_en.append('<a class="nav-link" href="' + language_data[2].link  + '"><i class="' + language_data[2].icon + '"></i>' + language_data[2].title.en + '</a>');
+    navitem3_en.append('<a class="nav-link" href="' + language_data[3].link  + '"><i class="' + language_data[3].icon + '"></i>' + language_data[3].title.en + '</a>');
+    navitem4_en.append('<a class="nav-link" href="' + language_data[4].link  + '"><i class="' + language_data[4].icon + '"></i>' + language_data[4].title.en + '</a>');
 
     /* Adds the navitems to the unsorted list*/ 
+    ul_sv.append(navitem5_sv);
     ul_sv.append(navitem1_sv);
     ul_sv.append(navitem2_sv);
     ul_sv.append(navitem3_sv);
     ul_sv.append(navitem4_sv);
 
+    ul_en.append(navitem5_en);
     ul_en.append(navitem1_en);
     ul_en.append(navitem2_en);
     ul_en.append(navitem3_en);
