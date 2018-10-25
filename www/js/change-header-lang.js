79c8dd9af8c38ebe9d44c9b47f9bfc27bc25894f
SimonPalmgren <simon.palmgren91@gmail.com>
2018-10-25 10:36:47 +0200

    changed name on website

--- www/js/change-header-lang.js
+++ www/js/change-header-lang.js
@@ -1,9 +1,9 @@
 
 $('.svflag').click(function(){
     $('h1 span:first-child').text('Detta är');
-    rotatingText.toRotate = ['breakout online', 'BREAKOUT ONLINE'];
+    rotatingText.toRotate = ['classic brekout', 'CLASSIC BREAKOUT'];
 });
 $('.ukflag').click(function(){
      $('h1 span:first-child').text('This is');
-     rotatingText.toRotate = ['breakout online', 'BREAKOUT ONLINE'];
+     rotatingText.toRotate = ['classic brekout', 'CLASSIC BREAKOUT'];
 });
