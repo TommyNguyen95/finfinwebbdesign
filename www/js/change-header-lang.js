d4cae1481b9bebfcabf4782741e061abf08a1429
SimonPalmgren <simon.palmgren91@gmail.com>
2018-10-25 19:07:43 +0200

    added border and styled border

--- www/js/change-header-lang.js
+++ www/js/change-header-lang.js
@@ -1,9 +1,9 @@
 
 $('.svflag').click(function(){
     $('h1 span:first-child').text('Detta är');
-    rotatingText.toRotate = ['classic brekout', 'CLASSIC BREAKOUT'];
+    rotatingText.toRotate = ['classic breakout', 'CLASSIC BREAKOUT'];
 });
 $('.ukflag').click(function(){
      $('h1 span:first-child').text('This is');
-     rotatingText.toRotate = ['classic brekout', 'CLASSIC BREAKOUT'];
+     rotatingText.toRotate = ['classic breakout', 'CLASSIC BREAKOUT'];
 });
