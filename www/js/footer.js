09be324071ec5e4a9b076dedeec20aa720796d73
Sudha Parimala <mailme.sudha.k@gmail.com>
2018-10-29 13:11:07 +0100

    changed index.html,footer.json and footer.js

--- www/js/footer.js
+++ www/js/footer.js
@@ -1,40 +1,36 @@
-$.getJSON('/json/footer.json', languageToggle);
+$.getJSON('/json/footer.json', write);
 
-function languageToggle (translate) {
+let language = "swedish";
 
-    // Loops through the first object in history.json-file
-	for( let language in translate){
+function write (data) {
 
-		// Creates a variable 'lang' to hold the propertyValue from 'translate'
-		let lang = translate[language];
+let x = 1;
+for(let icons in data){
 
-		// Creates two p-tags with class names "sv" and "en" from history.json-file
-		let textP1 = $('<p class="' + language + '"</p>');
-		let textP2 = $('<p class="' + language + '"</p>');
+	let icon = data[icons];
 
-		// Appends index[0] and index[1] from history.json in previous created variables
-		textP1.append('<p>' + lang[0] + '</p>');
-		textP2.append('<p>' + lang[1] + '</p>');
-
-		// Appends the p-tags to the classes text1 and text2, in history section
-		$('.text1').append(textP1);
-		$('.text2').append(textP2);
-
+	if(language=="swedish"){
+		$('.gname'+x).text(icon.sv);
 	}
-	
-	// Class 'en' is by default hidden
-    $('.en').hide();
-    
-    // When 'svflag' is clicked on 'sv' is shown and 'en' hidden
-	$('.svflag').click(function(){
-		$('.sv').show();
-		$('.en').hide();
-	});
+	else if(language=="english"){
+		$('.gname'+x).text(icon.en);
+	}
 
-    // When 'enflag' is clicked on 'en' is shown and 'sv' hidden
-	$('.ukflag').click(function(){
-		$('.sv').hide();
-		$('.en').show();
-	})
+}
+
+
+	
+
+
 }
  
+
+
+$('.svflag').click(function(){
+    language="swedish";
+    $.getJSON('/json/footer.json', write);
+});
+$('.ukflag').click(function(){
+     language="english";
+     $.getJSON('/json/footer.json', write);
+});
