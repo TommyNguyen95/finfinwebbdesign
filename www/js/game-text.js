e7ea3ec677451de7998f59441f745ea6b8341a27
SimonPalmgren <simon.palmgren91@gmail.com>
2018-10-29 15:05:32 +0100

    added text to json and added game-text.js
    styled ball and paddle

---
+++ www/js/game-text.js
@@ -0,0 +1,32 @@
+
+$.getJSON('/json/startpage.json', write);
+
+let language="swedish";
+
+function write(data){
+    let x=1;
+
+    for(let games in data){
+
+        let game= data[games];
+
+        if(language==="swedish"){
+            $('.gname'+x).text(game.sv);
+        }
+        else if(language==="english"){
+            $('.gname'+x).text(game.en);
+        }
+
+        x++;       
+    }
+}
+
+
+$('.svflag').click(function(){
+    language="swedish";
+    $.getJSON('/json/startpage.json', write);
+});
+$('.ukflag').click(function(){
+     language="english";
+     $.getJSON('/json/startpage.json', write);
+});
