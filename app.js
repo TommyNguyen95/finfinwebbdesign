4d14342dd73a88ef5cfd650f47009951d18f9a46
sunsneezer <xil_wong@hotmail.com>
2018-10-31 11:13:52 +0100

    highscore changes

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ for(let conf of config.sass){
 let fs= require('fs'); // import the fileSystem library
 let bodyParser = require('body-parser'); // import body-parser (to read sent data from clients)
 app.use(bodyParser.json()); // use body-parser
+app.use(bodyParser.urlencoded({ extended: false })); // configure body-parser
  
 let highscores = require('./highscore.json'); // load the json file - store it in a new variable
  
@@ -41,10 +42,10 @@ let highscores = require('./highscore.json'); // load the json file - store it i
 app.post('/add-score', (req, res) => {
   highscores.push(req.body); // add the new score
   highscores.sort(function(a,b){
-    // .... Check MDN js array sort
+     // Google MDN js array sort and write the sort-function
   });
   highscores = highscores.slice(0,10); // only keep the top 10 in the array
-  fs.writeFile('./highscore.json', highscores); // replace the file content with the new array
+  fs.writeFile('./highscore.json', JSON.stringify(highscores), ()=>{}); // replace the file content with the new array
   res.json(highscores); // respond to the browser, send the new/updated array
 });
 
--- www/js/highscore.js
+++ www/js/highscore.js
@@ -1,9 +1,11 @@
-$('./highscore.json').on('click', postNewHighscore);
+$('#submit-button').on('click', postNewHighscore);
  
 function postNewHighscore() {
-  let name = $();//......  fetch the name from your <input>/or otherwhere//
-  let score = $();//...... fetch the score from the game's "score"-variable//
-  $.post( "/add-score", { name, score }, function(responseData) {
+  console.log(87654);
+  
+  let name = $('#name');//......  fetch the name from your <input>/or otherwhere//
+  let score = $('#points');//...... fetch the score from the game's "score"-variable//
+  $.post( "/add-score", { "name": name, "score": score }, function(responseData) {
     console.log('the new highscore-list is:', responseData);
     console.error('append/use the new highscore-list then remove this console.error');
   });
