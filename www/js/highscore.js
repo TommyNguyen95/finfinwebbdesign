0fae4ce0f6d9240c51a5e9fd482d7fb12ab56227
sunsneezer <xil_wong@hotmail.com>
2018-10-31 11:22:56 +0100

    moved highscore.json

--- www/js/highscore.js
+++ www/js/highscore.js
@@ -3,8 +3,8 @@ $('#submit-button').on('click', postNewHighscore);
 function postNewHighscore() {
   console.log(87654);
   
-  let name = $('#name');//......  fetch the name from your <input>/or otherwhere//
-  let score = $('#points');//...... fetch the score from the game's "score"-variable//
+  let name = $('#name').val();//......  fetch the name from your <input>/or otherwhere//
+  let score = $('#points').val();//...... fetch the score from the game's "score"-variable//
   $.post( "/add-score", { "name": name, "score": score }, function(responseData) {
     console.log('the new highscore-list is:', responseData);
     console.error('append/use the new highscore-list then remove this console.error');
