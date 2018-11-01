c3df7fea12c97d1bc931a293f8db944f1c34398f
sunsneezer <xil_wong@hotmail.com>
2018-11-01 11:46:42 +0100

    highscore update

--- www/js/highscore.js
+++ www/js/highscore.js
@@ -1,18 +1,44 @@
 $('#submit-button').on('click', postNewHighscore);
+
  
 function postNewHighscore() {
-  console.log(87654);
-  
+
   let name = $('#name').val();//......  fetch the name from your <input>/or otherwhere//
   let score = $('#points').val();//...... fetch the score from the game's "score"-variable//
   $.post( "/add-score", { "name": name, "score": score }, function(responseData) {
     appendHighscoreList(responseData);
+
+
   });
 }
 
-
 function appendHighscoreList(highscores) {
   console.log('the new highscore-list is:', highscores);
-  $('tbody').empty();
   
+  $('tbody').empty();
+
 }
+
+
+$.getJSON('/json/highscore.json',highscoreTable);
+
+function highscoreTable(namesAndScores){
+ 
+ 
+  let i = 1;
+
+  for(let nas of namesAndScores){
+
+    let tr = $('<tr/>');
+
+    tr.append('<td>'+ i +'</td>'),
+    tr.append('<td>'+ nas.name +'</td>'),
+    tr.append('<td>'+ nas.score +'</td>')
+
+    $('tbody').append(tr);
+    i++;
+ }
+
+}
+
+
