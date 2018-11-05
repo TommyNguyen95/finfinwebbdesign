0e2209eace9a917e27eab000c9b455c1e563ae11
Tommy <tommynguyen@live.se>
2018-11-05 10:06:19 +0100

    format write-startpage.js

--- www/js/write-startpage-json.js
+++ www/js/write-startpage-json.js
@@ -1,33 +1,29 @@
-
 $.getJSON('/json/startpage.json', write);
 
-let language="swedish";
+let language = "swedish";
 
-function write(data){
-    let x=1;
+function write(data) {
+  let x = 1;
 
-    for(let games in data){
-
-        let game= data[games];
-
-        if(language==="swedish"){
-            $('.gname'+x).text(game.sv);
-        }
-        else if(language==="english"){
-            $('.gname'+x).text(game.en);
-        }
-
-        x++;       
+  for (let games in data) {
+    let game = data[games];
+    if (language === "swedish") {
+      $('.gname' + x).text(game.sv);
     }
+    else if (language === "english") {
+      $('.gname' + x).text(game.en);
+    }
+
+    x++;
+  }
 }
 
-
-$('.svflag').click(function(){
-    language="swedish";
-    $.getJSON('/json/startpage.json', write);
+$('.svflag').click(function () {
+  language = "swedish";
+  $.getJSON('/json/startpage.json', write);
 });
-$('.ukflag').click(function(){
-     language="english";
-     $.getJSON('/json/startpage.json', write);
+$('.ukflag').click(function () {
+  language = "english";
+  $.getJSON('/json/startpage.json', write);
 });
 
